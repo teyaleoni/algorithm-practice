b01956c3dcd9efdf24a5a191ef288c87b91131a6
Teya Urena <teyaleoni@gmail.com>
2020-10-27 09:30:52 -0700

    add question 1

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1. https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+function sockMerchant(n, ar) {
+  let sorted = ar.sort((a, b) => a - b);
+  let pairs = 0;
+
+  for (let i = 0; i < n - 1; i++) {
+    if (sorted[i] === sorted[i + 1]) {
+      pairs++;
+      i += 1;
+    }
+  }
+  return pairs;
+}
+
+// 2. https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
